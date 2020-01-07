ffea4ccc47cd7a3ed8fd5465115ae185ed43241a
Beth Dunford <eag732@gmail.com>
2020-01-06 19:00:09 -0600

    finished afternoon project

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -13,7 +13,7 @@ export default class Body extends React.Component {
                 <link href="https://fonts.googleapis.com/css?family=Courgette|Kaushan+Script|Montserrat&display=swap" rel="stylesheet"></link>
                 <h2>Welcome To Our Studio</h2>
                 <h1>IT'S NICE TO MEET YOU</h1>
-                <button>TELL ME MORE</button>
+                <button id="more-button">TELL ME MORE</button>
             </body>
         )
     }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Header.css';
-// import hamburger from '../menu-icon.png';
 
 export default class Header extends React.Component {
     constructor() {
@@ -24,24 +23,23 @@ export default class Header extends React.Component {
                 <nav>
                     <h3>Start Bootstrap</h3>
                     <ul id='site-nav'>
-                        <li className='menu-text'>Services</li>
-                        <li className='menu-text'>Portfolio</li>
-                        <li className='menu-text'>About</li>
-                        <li className='menu-text'>Team</li>
-                        <li className='menu-text'>Contact</li>
+                        <li className='menu-text'>SERVICES</li>
+                        <li className='menu-text'>PORTFOLIO</li>
+                        <li className='menu-text'>ABOUT</li>
+                        <li className='menu-text'>TEAM</li>
+                        <li className='menu-text'>CONTACT</li>
                         <li>
-                            <img
+                        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+                            <button
                                 onClick={this.handleClick}
-                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Menu%2C_Web_Fundamentals_%28White%29.svg/1280px-Menu%2C_Web_Fundamentals_%28White%29.svg.png"
-                                alt="Menu icon"
-                            />
+                                >MENU <i class="fa fa-bars"></i></button>
                         </li>
                         <ul className={this.state.menuStatus}>
-                            <li>Services</li>
-                            <li>Portfolio</li>
-                            <li>About</li>
-                            <li>Team</li>
-                            <li>Contact</li>
+                            <li>SERVICES</li>
+                            <li>PORTFOLIO</li>
+                            <li>ABOUT</li>
+                            <li>TEAM</li>
+                            <li>CONTACT</li>
                         </ul>
                     </ul>
                 </nav>
